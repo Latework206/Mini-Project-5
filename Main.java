da396f4595cecd63c6963823df4c20a3e2b7c2b1
John Sheffield <john.sheffield@patelhs.org>
2021-02-25 18:17:25 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Hello! Welcome to PHS. Lets calculate your avrage grade. How many classes do you have? ");
+    int ave = scan.nextInt();
+  }
+}
