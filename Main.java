f63936320766cafdcbe63522c997f59da4a47209
Latework206 <71711038+Latework206@users.noreply.github.com>
2021-03-31 11:54:17 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,20 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    System.out.println("Hello! Welcome to PHS. Lets calculate your avrage grade. How many classes do you have? ");
-    int ave = scan.nextInt();
+    System.out.println("How tall is the world's tallest building in feet?");
+    int height = scan.nextInt();
+    findHeight(height);
+  }
+
+  static boolean findHeight(int height) {
+    Scanner scan = new Scanner(System.in);
+    if (height == 2722) {
+      System.out.println("You are correct!");
+      return true;
+    } else {
+      System.out.println("That is incorrect! Try again! (Hint, it's over 2000)");
+      height = scan.nextInt();
+      return findHeight(height);
+    }
   }
 }
